6c44639110aaead77e2ec0e2eaf73e20376f11dd
GBTC441010UR <GBTC441010UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-01-28 12:38:43 -0500

    Changed secretword to 5

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Scanner keyboard = new Scanner(System.in);
+        int secretNumber, guess = 0;
+
+        secretNumber = 5;
+
+        System.out.println("I'm thinking of a number between 1 and 1000");
+        System.out.println("Enter the number:");
+        guess = keyboard.nextInt();
+
+        while (guess != secretNumber) {
+            System.out.println("\nYou are wrong. Try again.");
+            System.out.println("Enter the number: ");
+            guess = keyboard.nextInt();
+
+        }
+        System.out.println("You are correct. You win a  prize!");
+        keyboard.close();
+    }
+}
